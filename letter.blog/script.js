138dccd4233a5f323d2371bb9cd06ca1409646db
marythea <92129922+marythea@users.noreply.github.com>
2024-05-29 00:15:11 +0800

    Add files via upload

---
+++ letter.blog/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $("#messageState").on("change", function () {
+        $(".message").removeClass("openNor closeNor");
+        if ($("#messageState").is(":checked")) {
+            $(".message")
+                .removeClass("closed no-anim")
+                .addClass("openNor");
+            $(".heart")
+                .removeClass("closeHer openedHer")
+                .addClass("openHer");
+            $(".container").stop().animate({ backgroundColor: "#f48fb1" }, 2000);
+            console.log("Opening");
+        } else {
+            $(".message").removeClass("no-anim").addClass("closeNor");
+            $(".heart")
+                .removeClass("openHer openedHer")
+                .addClass("closeHer");
+            $(".container").stop().animate({ backgroundColor: "#fce4ec" }, 2000);
+            console.log("Closing");
+        }
+    });
+
+    $(".message").on(
+        "webkitAnimationEnd oanimationend msAnimationEnd animationend",
+        function () {
+            console.log("Message Animation End");
+            if ($(".message").hasClass("closeNor")) {
+                $(".message").addClass("closed");
+            }
+            $(".message")
+                .removeClass("openNor closeNor")
+                .addClass("no-anim");
+        }
+    );
+
+    $(".heart").on(
+        "webkitAnimationEnd oanimationend msAnimationEnd animationend",
+        function () {
+            console.log("Heart Animation End");
+            if (!$(".heart").hasClass("closeHer")) {
+                $(".heart").addClass("openedHer beating");
+            } else {
+                $(".heart").addClass("no-anim").removeClass("beating");
+            }
+            $(".heart").removeClass("openHer closeHer");
+        }
+    );
+});
